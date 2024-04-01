6f74ca2f1a4033ea5e312f5017d8d7f72a0827b3
Panson <PansonPanson@163.com>
2024-04-01 14:24:06 +0800

    update weekly

--- 2024/weekly-20.md
+++ 2024/weekly-20.md
@@ -18,6 +18,32 @@ _________________
 
 # 3 Algorithm(算法题)
 
++ [239. 滑动窗口最大值](https://leetcode.cn/problems/sliding-window-maximum/description/)
+```
+public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> q = new ArrayDeque<>();
+        int index = 0;
+        for(int i = 0; i < n; i++) {
+            while(!q.isEmpty() && nums[q.peekLast()] <= nums[i]) {
+                q.pollLast();
+            }
+            while(!q.isEmpty() && i - q.peekFirst() >= k) {
+                q.pollFirst();
+            }
+            q.offerLast(i);
+
+            if(i >= k - 1) {
+                res[i - k + 1] = nums[q.peekFirst()];
+            }
+        }
+
+        return res;
+    }
+
+```
+
 
 
 
--- leetcode-in-java/src/main/java/top/panson/injava/tags/monotonicqueue/L239.java
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/monotonicqueue/L239.java
@@ -1,5 +1,7 @@
 package top.panson.injava.tags.monotonicqueue;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.PriorityQueue;
 
 /**
@@ -35,5 +37,38 @@ public class L239 {
             }
             return res;
         }
+    }
+
+    /**
+     * 使用单调队列，更优
+     * @param nums
+     * @param k
+     * @return
+     */
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] res = new int[n - k + 1];
+        Deque<Integer> q = new ArrayDeque<>();
+        int index = 0;
+        for(int i = 0; i < n; i++) {
+            while(!q.isEmpty() && nums[q.peekLast()] <= nums[i]) {
+                q.pollLast();
+            }
+            while(!q.isEmpty() && i - q.peekFirst() >= k) {
+                q.pollFirst();
+            }
+            q.offerLast(i);
+
+            if(i >= k - 1) {
+                res[i - k + 1] = nums[q.peekFirst()];
+            }
+        }
+
+        return res;
+    }
+
+
+    public static void main(String[] args) {
+        maxSlidingWindow(new int[]{7, 2,,4})
     }
 }
