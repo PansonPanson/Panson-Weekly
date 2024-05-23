e02d6bd5cf42fec2b129b2c123800f92385e07a2
Panson <PansonPanson@163.com>
2024-05-23 23:58:32 +0800

    update weekly

--- 2024/weekly-26.md
+++ 2024/weekly-26.md
@@ -303,6 +303,37 @@ class Solution {
 
 }
 ```
++ [876. 链表的中间结点](https://leetcode.cn/problems/middle-of-the-linked-list/description/)
+
+```java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode p1 = dummy;
+        ListNode p2 = dummy;
+        while(p2 != null && p2.next != null) {
+            p1 = p1.next;
+            p2 = p2.next.next;
+        }
+        if(p2 == null) {
+            return p1;
+        } else {
+            return p1.next;
+        }
+    }
+}
+```
 
 
 
---
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/twopointers/L0876.java
@@ -0,0 +1,37 @@
+package top.panson.injava.tags.twopointers;
+
+import top.panson.injava.question.commondata.ListNode;
+
+/**
+ * @author Panson
+ * @create 2024-05-23
+ */
+public class L0876 {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode() {}
+     *     ListNode(int val) { this.val = val; }
+     *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+    class Solution {
+        public ListNode middleNode(ListNode head) {
+            ListNode dummy = new ListNode(-1);
+            dummy.next = head;
+            ListNode p1 = dummy;
+            ListNode p2 = dummy;
+            while(p2 != null && p2.next != null) {
+                p1 = p1.next;
+                p2 = p2.next.next;
+            }
+            if(p2 == null) {
+                return p1;
+            } else {
+                return p1.next;
+            }
+        }
+    }
+}
