5f653136762dd9c520a873a7235a40088364d8cb
Panson <PansonPanson@163.com>
2024-06-02 16:25:20 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/other/Stack2Queue.java
@@ -0,0 +1,55 @@
+package top.panson.injava.other;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Panson
+ * @create 2024-05-31
+ */
+public class Stack2Queue<T> {
+
+    private Deque<T> stackA;
+
+    private Deque<T> stackB;
+
+    public Stack2Queue() {
+        stackA = new ArrayDeque<>();
+        stackB = new ArrayDeque<>();
+    }
+
+    public boolean isEmpty() {
+        return stackA.isEmpty();
+    }
+
+    public void offer(T element) {
+        stackA.push(element);
+    }
+
+    public T pop() {
+        if(isEmpty()) {
+            throw new RuntimeException("Stack2Queue is empty");
+        }
+        if(stackB.isEmpty()) {
+            while (!stackA.isEmpty()) {
+                stackB.push(stackA.pop());
+            }
+        }
+        return stackB.pop();
+    }
+
+    public T peek() {
+        if(isEmpty()) {
+            throw new RuntimeException("Stack2Queue is empty");
+        }
+        if(isEmpty()) {
+            throw new RuntimeException("Stack2Queue is empty");
+        }
+        if(stackB.isEmpty()) {
+            while (!stackA.isEmpty()) {
+                stackB.push(stackA.pop());
+            }
+        }
+        return stackB.peek();
+    }
+}
