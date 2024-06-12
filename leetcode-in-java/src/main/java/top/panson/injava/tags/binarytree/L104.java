df6190553892dd5c0db68bc225cb08c8fd1a3c57
Panson <PansonPanson@163.com>
2024-06-12 20:33:22 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/tags/binarytree/L104.java
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/binarytree/L104.java
@@ -1,10 +1,19 @@
 package top.panson.injava.tags.binarytree;
 
+import top.panson.injava.question.commondata.TreeNode;
+
 /**
  * @author Panson
  * @create 2024-06-04
  */
 public class L104 {
 
-
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if(root == null) {
+                return 0;
+            }
+            return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        }
+    }
 }
