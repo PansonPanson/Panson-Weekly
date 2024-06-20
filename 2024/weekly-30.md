cfc013949b00257fbdc83c27bb25b2dac4b5d2c5
Panson <PansonPanson@163.com>
2024-06-20 18:51:51 +0800

    update weekly

--- 2024/weekly-30.md
+++ 2024/weekly-30.md
@@ -68,6 +68,32 @@ class Solution {
 }
 ```
 
++ [90. 子集 II](https://leetcode.cn/problems/subsets-ii/description/)
+```java
+class Solution {
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> track = new LinkedList<>();
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        backtrack(nums, 0);
+        return res;
+    }
+
+    public void backtrack(int[] nums, int start) {
+        res.add(new LinkedList<>(track));
+
+        for(int i = start; i < nums.length; i++) {
+            if(i > start && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            track.add(nums[i]);
+            backtrack(nums, i + 1);
+            track.removeLast();
+        }
+    }
+}
+```
+
 
 
 
---
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/backtracking/L090.java
@@ -0,0 +1,35 @@
+package top.panson.injava.tags.backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Panson
+ * @create 2024-06-20
+ */
+public class L090 {
+    class Solution {
+        List<List<Integer>> res = new ArrayList<>();
+        LinkedList<Integer> track = new LinkedList<>();
+        public List<List<Integer>> subsetsWithDup(int[] nums) {
+            Arrays.sort(nums);
+            backtrack(nums, 0);
+            return res;
+        }
+
+        public void backtrack(int[] nums, int start) {
+            res.add(new LinkedList<>(track));
+
+            for(int i = start; i < nums.length; i++) {
+                if(i > start && nums[i] == nums[i - 1]) {
+                    continue;
+                }
+                track.add(nums[i]);
+                backtrack(nums, i + 1);
+                track.removeLast();
+            }
+        }
+    }
+}
