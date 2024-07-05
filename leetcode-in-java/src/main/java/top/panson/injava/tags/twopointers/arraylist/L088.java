ece957937e6c45d07c73a0f38d881cc3e61479c5
Panson <PansonPanson@163.com>
2024-07-05 09:38:56 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/twopointers/arraylist/L088.java
@@ -0,0 +1,26 @@
+package top.panson.injava.tags.twopointers.arraylist;
+
+/**
+ * @author Panson
+ * @create 2024-07-04
+ */
+public class L088 {
+    class Solution {
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int p1 = m - 1;
+            int p2 = n - 1;
+            int i = m + n - 1;
+            while(p1 >= 0 && p2 >= 0) {
+                if(nums1[p1] > nums2[p2]) {
+                    nums1[i--] = nums1[p1--];
+                } else {
+                    nums1[i--] = nums2[p2--];
+                }
+            }
+
+            while (p2 >= 0) {
+                nums1[i--] = nums2[p2--];
+            }
+        }
+    }
+}
