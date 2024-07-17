239933ccc79a45a3eba93369b6c0649f22f29bcd
Panson <PansonPanson@163.com>
2024-07-17 22:52:14 +0800

    update weekly

--- 2024/weekly-34.md
+++ 2024/weekly-34.md
@@ -94,6 +94,45 @@ class NumMatrix {
  */
 ```
 
++ [1081. 不同字符的最小子序列](https://leetcode.cn/problems/smallest-subsequence-of-distinct-characters/description/)
+```java
+class Solution {
+    public String smallestSubsequence(String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        boolean[] exist = new boolean[255];
+        int[] count = new int[255];
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)]++;
+        }
+
+        for(int i = 0; i < s.length(); i++) {
+            char cur = s.charAt(i);
+            count[cur]--;
+            if(exist[cur]) {
+                continue;
+            }
+
+            while(!stack.isEmpty() && stack.peek() > cur) {
+                if(count[stack.peek()] == 0) {
+                    break;
+                }
+                exist[stack.pop()] = false;
+            }
+            stack.push(cur);
+            exist[cur] = true;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while(!stack.isEmpty()) {
+            sb.append(stack.pop());
+        }
+
+        return sb.reverse().toString();
+
+    }
+} 
+```
+
 
 
 
---
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/arrays/L1081.java
@@ -0,0 +1,47 @@
+package top.panson.injava.tags.arrays;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * @author Panson
+ * @create 2024-07-17
+ */
+public class L1081 {
+
+    class Solution {
+        public String smallestSubsequence(String s) {
+            Deque<Character> stack = new ArrayDeque<>();
+            boolean[] exist = new boolean[255];
+            int[] count = new int[255];
+            for(int i = 0; i < s.length(); i++) {
+                count[s.charAt(i)]++;
+            }
+
+            for(int i = 0; i < s.length(); i++) {
+                char cur = s.charAt(i);
+                count[cur]--;
+                if(exist[cur]) {
+                    continue;
+                }
+
+                while(!stack.isEmpty() && stack.peek() > cur) {
+                    if(count[stack.peek()] == 0) {
+                        break;
+                    }
+                    exist[stack.pop()] = false;
+                }
+                stack.push(cur);
+                exist[cur] = true;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            while(!stack.isEmpty()) {
+                sb.append(stack.pop());
+            }
+
+            return sb.reverse().toString();
+
+        }
+    }
+}
