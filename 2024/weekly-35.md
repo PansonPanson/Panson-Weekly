f9e980e072742f1ff92112619254470257ec56b0
Panson <PansonPanson@163.com>
2024-07-23 12:54:00 +0800

    update weekly

--- 2024/weekly-35.md
+++ 2024/weekly-35.md
@@ -111,6 +111,37 @@ class Solution {
 ```
 
 
++ [523. 连续的子数组和](https://leetcode.cn/problems/continuous-subarray-sum/description/)
+```java
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int[] preSum = new int[n + 1];
+        preSum[0] = 0;
+        for(int i = 0; i < n; i++) {
+            preSum[i + 1] = preSum[i] + nums[i];
+        }
+        System.out.println(Arrays.toString(preSum));
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 1; i < preSum.length; i++) {
+            int remain = preSum[i] % k;
+            if(remain == 0 && i >= 2) {
+                return true;
+            } else {
+                if(map.containsKey(remain) && (i - map.get(remain)) >= 2) {
+                    return true;
+                }
+            }
+
+            map.putIfAbsent(remain, i);
+
+        }
+        return false;
+    }
+} 
+```
+
+
 
 
 
---
+++ leetcode-in-java/src/main/java/top/panson/injava/tags/presum/L523.java
@@ -0,0 +1,39 @@
+package top.panson.injava.tags.presum;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Panson
+ * @create 2024-07-23
+ */
+public class L523 {
+
+    class Solution {
+        public boolean checkSubarraySum(int[] nums, int k) {
+            int n = nums.length;
+            int[] preSum = new int[n + 1];
+            preSum[0] = 0;
+            for(int i = 0; i < n; i++) {
+                preSum[i + 1] = preSum[i] + nums[i];
+            }
+            System.out.println(Arrays.toString(preSum));
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int i = 1; i < preSum.length; i++) {
+                int remain = preSum[i] % k;
+                if(remain == 0 && i >= 2) {
+                    return true;
+                } else {
+                    if(map.containsKey(remain) && (i - map.get(remain)) >= 2) {
+                        return true;
+                    }
+                }
+
+                map.putIfAbsent(remain, i);
+
+            }
+            return false;
+        }
+    }
+}
