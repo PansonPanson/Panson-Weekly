c75dcb17e48c80c9ef2ab39c917e4ed5c6d0a2ba
Panson <PansonPanson@163.com>
2025-01-12 15:37:12 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/L070.java
+++ leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/L070.java
@@ -21,4 +21,26 @@ public class L070 {
             return dp[n];
         }
     }
+
+
+    class Solution1 {
+
+        public int climbStairs(int n) {
+
+            if(n <= 2) {
+                return n;
+            }
+
+            int pre_pre = 1;
+            int pre = 2;
+            int cur = 3;
+            for(int i = 3; i <= n; i++) {
+                cur = pre + pre_pre;
+                int tmp = pre;
+                pre = cur;
+                pre_pre = tmp;
+            }
+            return cur;
+        }
+    }
 }
