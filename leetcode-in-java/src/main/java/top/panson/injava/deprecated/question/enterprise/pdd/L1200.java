f48d84e685082235cb5fcde95c7dc90d07b3e20c
Panson <PansonPanson@163.com>
2025-01-14 07:52:06 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/L1200.java
+++ leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/L1200.java
@@ -38,4 +38,28 @@ public class L1200 {
             return res;
         }
     }
+
+
+    class Solution1 {
+        public List<List<Integer>> minimumAbsDifference(int[] arr) {
+            // 排序
+            // 遍历，维护当前最小的元素差和对应元素差的元素对列表
+            Arrays.sort(arr);
+            int min = Integer.MAX_VALUE;
+            List<List<Integer>> res = new ArrayList<>();
+            for(int i = 1; i < arr.length; i++) {
+                if(arr[i] - arr[i - 1] > min) {
+                    continue;
+                } else if(arr[i] - arr[i - 1] < min) {
+                    min = arr[i] - arr[i - 1];
+                    res.clear();
+                }
+                List<Integer> cur = new ArrayList<>();
+                cur.add(arr[i - 1]);
+                cur.add(arr[i]);
+                res.add(cur);
+            }
+            return res;
+        }
+    }
 }
