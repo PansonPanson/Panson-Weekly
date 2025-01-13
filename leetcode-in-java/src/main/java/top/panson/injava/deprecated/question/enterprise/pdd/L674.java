97e1b22fdd25fd4d06a0880969a629f35296bfef
Panson <PansonPanson@163.com>
2025-01-13 19:14:49 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/L674.java
@@ -0,0 +1,22 @@
+package top.panson.injava.deprecated.question.enterprise.pdd;
+
+/**
+ * @author Panson
+ * @create 2025-01-13
+ */
+public class L674 {
+    class Solution {
+        public int findLengthOfLCIS(int[] nums) {
+            int start = 0;
+            int max = 1;
+            for(int end = 1; end < nums.length; end++) {
+                if(nums[end] > nums[end - 1]) {
+                    max = Math.max(max, end - start + 1);
+                } else {
+                    start = end;
+                }
+            }
+            return max;
+        }
+    }
+}
