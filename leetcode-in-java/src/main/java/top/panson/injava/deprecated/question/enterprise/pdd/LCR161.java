14c4f31ca740875f0da1969f4a2aae8fcb4c7f59
Panson <PansonPanson@163.com>
2025-01-13 18:25:47 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/LCR161.java
+++ leetcode-in-java/src/main/java/top/panson/injava/deprecated/question/enterprise/pdd/LCR161.java
@@ -23,4 +23,23 @@ public class LCR161 {
             return max;
         }
     }
+
+    class Solution1 {
+        public int maxSales(int[] sales) {
+            // 动态规划：以某一天为结尾的连续子数组最大和
+            // dp[i] = dp[i - 1] > 0 ? dp[i - 1] + sales[i] : sales[i];
+            if(sales.length == 1) {
+                return sales[0];
+            }
+
+            int pre = sales[0];
+            int max = pre;
+            for(int i = 1; i < sales.length; i++) {
+                int cur = pre > 0 ? pre + sales[i] : sales[i];
+                pre = cur;
+                max = Math.max(cur, max);
+            }
+            return max;
+        }
+    }
 }
