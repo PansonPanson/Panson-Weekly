b57d7017f5572a7f17f13b3826da4bccf9973272
Panson <PansonPanson@163.com>
2025-01-15 08:15:20 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L104.java
@@ -0,0 +1,36 @@
+package top.panson.injava.code.question.enterprise.pdd;
+
+import top.panson.injava.tags.commondata.TreeNode;
+
+/**
+ * @author Panson
+ * @create 2025-01-15
+ */
+public class L104 {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public int maxDepth(TreeNode root) {
+            if(root == null) {
+                return 0;
+            }
+            int leftDepth = maxDepth(root.left);
+            int rightDepth = maxDepth(root.right);
+            return Math.max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
