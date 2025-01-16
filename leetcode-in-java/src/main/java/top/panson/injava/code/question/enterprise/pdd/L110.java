b62e4951398ceb83ecb1686c937bb63e359ed417
Panson <PansonPanson@163.com>
2025-01-17 07:41:37 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L110.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L110.java
@@ -44,5 +44,41 @@ public class L110 {
         }
 
 
+
+
+        /**
+         * Definition for a binary tree node.
+         * public class TreeNode {
+         *     int val;
+         *     TreeNode left;
+         *     TreeNode right;
+         *     TreeNode() {}
+         *     TreeNode(int val) { this.val = val; }
+         *     TreeNode(int val, TreeNode left, TreeNode right) {
+         *         this.val = val;
+         *         this.left = left;
+         *         this.right = right;
+         *     }
+         * }
+         */
+        class Solution1{
+            public boolean isBalanced(TreeNode root) {
+                return depth(root) >= 0;
+            }
+
+            public int depth(TreeNode root) {
+                if(root == null) {
+                    return 0;
+                }
+                int leftDepth = depth(root.left);
+                int rightDepth = depth(root.right);
+                if(leftDepth  == -1 || rightDepth == -1 || Math.abs(leftDepth - rightDepth) > 1) {
+                    return -1;
+                } else {
+                    return Math.max(leftDepth, rightDepth) + 1;
+                }
+
+            }
+        }
     }
 }
