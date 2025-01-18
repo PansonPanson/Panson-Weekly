c2d2f8f4728c3e1a86c8009458a781bd874926b0
Panson <PansonPanson@163.com>
2025-01-18 14:58:17 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L617.java
@@ -0,0 +1,39 @@
+package top.panson.injava.code.question.enterprise.pdd;
+
+import top.panson.injava.tags.commondata.TreeNode;
+
+/**
+ * @author Panson
+ * @create 2025-01-18
+ */
+public class L617 {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    class Solution {
+        public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+            if(root1 == null) {
+                return root2;
+            }
+            if(root2 == null) {
+                return root1;
+            }
+            TreeNode newRoot = new TreeNode(root1.val + root2.val);
+            newRoot.left = mergeTrees(root1.left, root2.left);
+            newRoot.right = mergeTrees(root1.right, root2.right);
+            return newRoot;
+        }
+    }
+}
