5f38ec5de628c73bc02a2e5b4e76fa930784fb4e
Panson <PansonPanson@163.com>
2025-01-18 20:51:38 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L590.java
@@ -0,0 +1,66 @@
+package top.panson.injava.code.question.enterprise.pdd;
+
+import top.panson.injava.tags.commondata.Node;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Panson
+ * @create 2025-01-18
+ */
+public class L590 {
+
+    /*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+}
+*/
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    class Solution {
+        public List<Integer> postorder(Node root) {
+            List<Integer> res = new ArrayList<>();
+            traverse(root, res);
+            return res;
+        }
+
+        public void traverse(Node root, List<Integer> res) {
+            if(root == null) {
+                return;
+            }
+            for(Node child : root.children) {
+                traverse(child, res);
+            }
+            res.add(root.val);
+        }
+    }
+}
