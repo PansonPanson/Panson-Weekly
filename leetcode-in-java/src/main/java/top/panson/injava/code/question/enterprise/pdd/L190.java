475412919aaf5fbc85144006e432829fa05e77fb
Panson <PansonPanson@163.com>
2025-01-20 17:56:52 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L190.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L190.java
@@ -11,4 +11,22 @@ public class L190 {
             return Integer.reverse(n);
         }
     }
+
+
+    public class Solution1 {
+        // you need treat n as an unsigned value
+        public int reverseBits(int n) {
+            int res = 0;
+            for(int i = 0; i < 32; i++) {
+                // 按位与&的作用：当两个操作数的对应位都为1时，才是1
+                // 此处取最后一位
+                int last = n & 1;
+                // 按位或｜的作用：当两个操作数的对应位至少有一个为1，则为1
+                // 倒序
+                res = res << 1 | last;
+                n = n >> 1;
+            }
+            return res;
+        }
+    }
 }
