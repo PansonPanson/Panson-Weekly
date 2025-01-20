9527d157a46879acb5a9b25746ca1b6701d59ede
Panson <PansonPanson@163.com>
2025-01-20 17:47:39 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L190.java
@@ -0,0 +1,14 @@
+package top.panson.injava.code.question.enterprise.pdd;
+
+/**
+ * @author Panson
+ * @create 2025-01-20
+ */
+public class L190 {
+    public class Solution {
+        // you need treat n as an unsigned value
+        public int reverseBits(int n) {
+            return Integer.reverse(n);
+        }
+    }
+}
