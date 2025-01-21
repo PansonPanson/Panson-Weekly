3b6c06b796122a181d145d0d5cbe2e189d3eb517
Panson <PansonPanson@163.com>
2025-01-21 11:59:34 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L235.java
@@ -0,0 +1,83 @@
+package top.panson.injava.code.question.enterprise.pdd;
+
+import top.panson.injava.tags.commondata.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Panson
+ * @create 2025-01-21
+ */
+public class L235 {
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+
+    class Solution {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            List<TreeNode> pathP = getPath(root, p);
+            List<TreeNode> pathQ = getPath(root, q);
+            TreeNode ancestor = null;
+            for(int i = 0; i < pathP.size() && i < pathQ.size(); i++) {
+                if(pathP.get(i) == pathQ.get(i)) {
+                    ancestor = pathP.get(i);
+                } else {
+                    break;
+                }
+            }
+            return ancestor;
+
+        }
+
+
+        public List<TreeNode> getPath(TreeNode root, TreeNode target) {
+            List<TreeNode> path = new ArrayList<>();
+            TreeNode node = root;
+            while(node != target) {
+                path.add(node);
+                if(target.val < node.val) {
+                    node = node.left;
+                } else {
+                    node = node.right;
+                }
+            }
+            path.add(node);
+            return path;
+        }
+    }
+
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+
+    class Solution1 {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            TreeNode ancestor = root;
+            while(true) {
+                if(p.val < ancestor.val && q.val < ancestor.val) {
+                    ancestor = ancestor.left;
+                } else if(p.val > ancestor.val && q.val > ancestor.val) {
+                    ancestor = ancestor.right;
+                } else {
+                    break;
+                }
+            }
+            return ancestor;
+        }
+    }
+}
