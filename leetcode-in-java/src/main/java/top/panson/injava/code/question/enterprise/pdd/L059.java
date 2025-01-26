02b2bbbb086c7fd8b3202041fefb1adb86a91e59
Panson <PansonPanson@163.com>
2025-01-26 11:14:18 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L059.java
@@ -0,0 +1,47 @@
+package top.panson.injava.code.question.enterprise.pdd;
+
+/**
+ * @author Panson
+ * @create 2025-01-26
+ */
+public class L059 {
+
+    class Solution {
+        public int[][] generateMatrix(int n) {
+            int left = 0;
+            int right = n - 1;
+            int top = 0;
+            int bottom = n - 1;
+            int cur = 1;
+            int[][] res = new int[n][n];
+            while(cur <= n * n) {
+                for(int i = left; i <= right; i++) {
+                    res[top][i] = cur;
+                    cur++;
+                }
+                top++;
+                for(int i = top; i <= bottom; i++) {
+                    res[i][right] = cur;
+                    cur++;
+                }
+                right--;
+                if(top <= bottom) {
+                    for(int i = right; i >= left; i--) {
+                        res[bottom][i] = cur;
+                        cur++;
+                    }
+                }
+                bottom--;
+
+                if(left <= right) {
+                    for(int i = bottom; i >= top; i--) {
+                        res[i][left] = cur;
+                        cur++;
+                    }
+                }
+                left++;
+            }
+            return res;
+        }
+    }
+}
