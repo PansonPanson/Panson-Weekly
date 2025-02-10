a4ce8af25e65e75700477e859331ea1f86a6ee26
Panson <PansonPanson@163.com>
2025-02-10 11:28:43 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L413.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/enterprise/pdd/L413.java
@@ -23,4 +23,30 @@ public class L413 {
             return sum;
         }
     }
+
+
+
+    class Solution2 {
+        public int numberOfArithmeticSlices(int[] nums) {
+            // dp[i] : 以 dp[i] 为结尾的等差数列的个数
+            int n = nums.length;
+            if(n < 3) {
+                return 0;
+            }
+            int sum = 0;
+            int[] dp = new int[n];
+            // 当前等差数列个数
+            int dp_i = 0;
+
+            for(int i = 2; i < n; i++) {
+                if(nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2]) {
+                    dp_i = dp_i + 1;
+                } else {
+                    dp_i = 0;
+                }
+                sum += dp_i;
+            }
+            return sum;
+        }
+    }
 }
