f196df0a0515bf4d7231c5b7c35555f4889432b6
Panson <PansonPanson@163.com>
2025-02-22 21:42:05 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L72.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L72.java
@@ -1,5 +1,7 @@
 package top.panson.injava.code.question.tags.dp;
 
+import java.util.Arrays;
+
 /**
  * @author Panson
  * @create 2025-02-21
@@ -30,6 +32,50 @@ public class L72 {
                     // 替换
                     dp(s1, i - 1, s2, j - 1) + 1
             );
+        }
+
+        int min(int a, int b, int c) {
+            return Math.min(a, Math.min(b, c));
+        }
+    }
+
+
+    class Solution1 {
+        int[][] memo;
+        public int minDistance(String s1, String s2) {
+            int m = s1.length(), n = s2.length();
+            memo = new int[m][n];
+            for(int[] row : memo) {
+                Arrays.fill(row, -1);
+            }
+
+            // i，j 初始化指向最后一个索引
+            return dp(s1, m - 1, s2, n - 1);
+        }
+
+        // 定义：返回 s1[0..i] 和 s2[0..j] 的最小编辑距离
+        int dp(String s1, int i, String s2, int j) {
+            // base case
+            if (i == -1) return j + 1;
+            if (j == -1) return i + 1;
+
+            if(memo[i][j] != -1) {
+                return memo[i][j];
+            }
+            if (s1.charAt(i) == s2.charAt(j)) {
+                // 啥都不做
+                memo[i][j] = dp(s1, i - 1, s2, j - 1);
+            } else {
+                memo[i][j] = min(
+                        // 插入
+                        dp(s1, i, s2, j - 1) + 1,
+                        // 删除
+                        dp(s1, i - 1, s2, j) + 1,
+                        // 替换
+                        dp(s1, i - 1, s2, j - 1) + 1
+                );
+            }
+            return memo[i][j];
         }
 
         int min(int a, int b, int c) {
