216135c682ab6d5681aee84187897262bfad1d4b
Panson <PansonPanson@163.com>
2025-04-11 15:45:19 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/greedy/L55.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/greedy/L55.java
@@ -27,5 +27,18 @@ public class L55 {
         }
     }
 
+    public class Solution1 {
+        public boolean canJump(int[] nums) {
+            int maxReach = 0;
+
+            for (int i = 0; i < nums.length; i++) {
+                if (i > maxReach) return false;
+                maxReach = Math.max(maxReach, i + nums[i]);
+            }
+
+            return true;
+        }
+    }
+
 
 }
