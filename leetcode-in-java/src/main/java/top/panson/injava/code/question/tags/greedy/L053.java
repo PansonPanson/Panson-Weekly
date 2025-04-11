e98a473c9409ec2c00bc7449e9edb2168e4c4912
Panson <PansonPanson@163.com>
2025-04-11 15:04:58 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/greedy/L053.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/greedy/L053.java
@@ -8,5 +8,21 @@ package top.panson.injava.code.question.tags.greedy;
 //
 // 子数组是数组中的一个连续部分。
 public class L053 {
-
+    // dp[i] : 以  nums[i] 为结尾的最大连续子数组和
+    class Solution {
+        public int maxSubArray(int[] nums) {
+            if(nums.length == 0) {
+                return 0;
+            }
+            int pre = 0;
+            int cur = 0;
+            int res = nums[0];
+            for(int i = 0; i < nums.length; i++) {
+                cur = Math.max(pre + nums[i], nums[i]);
+                res = Math.max(res, cur);
+                pre = cur;
+            }
+            return res;
+        }
+    }
 }
