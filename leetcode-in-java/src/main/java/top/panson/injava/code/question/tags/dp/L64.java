75ce4a442148e2d647ddd832ebf1172c03f6284f
Panson <PansonPanson@163.com>
2025-04-16 23:33:44 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L64.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L64.java
@@ -34,4 +34,27 @@ public class L64 {
             return dp[m - 1][n - 1];
         }
     }
+
+
+    class Solution2 {
+        public int minPathSum(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            int[][] dp = new int[m][n];
+            for(int i = 0; i < m; i++) {
+                for(int j = 0; j < n; j++) {
+                    if(i == 0 && j == 0) {
+                        dp[i][j] = grid[i][j];
+                    } else if(i == 0) {
+                        dp[i][j] = dp[i][j - 1] + grid[i][j];
+                    } else if(j == 0) {
+                        dp[i][j] = dp[i - 1][j] + grid[i][j];
+                    } else {
+                        dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j]) + grid[i][j];
+                    }
+                }
+            }
+            return dp[m - 1][n - 1];
+        }
+    }
 }
