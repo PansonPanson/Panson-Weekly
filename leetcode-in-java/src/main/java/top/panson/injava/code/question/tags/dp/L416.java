6f353c2c510e3cee67831ad5509b5eef145bef7f
Panson <PansonPanson@163.com>
2025-04-17 16:59:10 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L416.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L416.java
@@ -32,4 +32,38 @@ public class L416 {
             return dp[n][sum];
         }
     }
+
+    class Solution1 {
+        public boolean canPartition(int[] nums) {
+            int sum = 0;
+            for(int num : nums) {
+                sum += num;
+            }
+            if(sum % 2 != 0) {
+                return false;
+            }
+            // dp[i][j] ： 表示前 i 个数中是否存在子集和为 j, return dp[nums.length + 1][sum / 2 + 1];
+            // for(0 n-1) for(1 n -2)……for(n-2)
+            boolean[][] dp = new boolean[nums.length + 1][sum / 2 + 1];
+            for(int i = 0; i <= nums.length; i++) {
+                for(int j = 0; j <= sum / 2; j++) {
+                    if(i == 0 && j == 0) {
+                        dp[i][j] = true;
+                    } else if(i == 0) {
+                        dp[i][j] = false;
+                    } else if(j == 0) {
+                        dp[i][j] = true;
+                    } else {
+                        if(j >= nums[i - 1]) {
+                            dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                        } else {
+                            dp[i][j] = dp[i - 1][j];
+                        }
+                    }
+                }
+            }
+            return dp[nums.length][sum / 2];
+
+        }
+    }
 }
