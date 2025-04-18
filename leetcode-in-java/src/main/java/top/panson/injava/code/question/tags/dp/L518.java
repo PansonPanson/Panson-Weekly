8bdd1ce35c20200d4e0907a3f406f79cfb6cd600
Panson <PansonPanson@163.com>
2025-04-18 10:49:04 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L518.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L518.java
@@ -28,4 +28,26 @@ public class L518 {
         }
     }
 
+    class Solution2 {
+        public int change(int amount, int[] coins) {
+            // dp[i][j] : 从前 i 个硬币中选择硬币，能凑出总金额 j 的组合数
+            int n = coins.length;
+            int[][] dp = new int[n + 1][amount + 1];
+            for(int i = 0; i <= n; i++) {
+                dp[i][0] = 1;
+            }
+
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= amount; j++) {
+                    if(j >= coins[i - 1]) {
+                        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[n][amount];
+        }
+    }
+
 }
