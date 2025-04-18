723ae4f20f5fb51db807e2cc9476c74e0a576198
Panson <PansonPanson@163.com>
2025-04-18 16:12:36 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L322.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L322.java
@@ -6,4 +6,27 @@ package top.panson.injava.code.question.tags.dp;
  */
 public class L322 {
 
+
+    class Solution {
+        public int coinChange(int[] coins, int amount) {
+            // dp[i][j] : 从前 i 种硬币中凑出总金额为 j 所需的最少硬币数
+            // dp[i][j] = Math.min(dp[i][j - coin] + 1, dp[i - 1][j]);
+            int n = coins.length;
+            int[][] dp = new int[n + 1][amount + 1];
+            for(int j = 1; j <= amount; j++) {
+                dp[0][j] = amount + 1;
+            }
+            for(int i = 1; i <= n; i++) {
+                for(int j = 1; j <= amount; j++) {
+                    if(j >= coins[i - 1]) {
+                        dp[i][j] = Math.min(dp[i][j - coins[i - 1]] + 1, dp[i - 1][j]);
+                    } else {
+                        dp[i][j] =  dp[i - 1][j];
+                    }
+                }
+            }
+            return dp[n][amount] > amount ? -1: dp[n][amount];
+        }
+    }
+
 }
