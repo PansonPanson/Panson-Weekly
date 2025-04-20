93e65f02619df74da6718cd5b8a37ac05b42e6c6
Panson <PansonPanson@163.com>
2025-04-20 16:50:47 +0800

    update weekly

---
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L674.java
@@ -0,0 +1,26 @@
+package top.panson.injava.code.question.tags.dp;
+
+/**
+ * @author Panson
+ * @create 2025-04-20
+ */
+public class L674 {
+    class Solution {
+        public int findLengthOfLCIS(int[] nums) {
+            int n = nums.length;
+            int[] dp = new int[n];
+            dp[0] = 1;
+            int max = 1;
+            for(int i = 1; i < n; i++) {
+                if(nums[i] > nums[i - 1]) {
+                    dp[i] = dp[i - 1] + 1;
+                } else {
+                    dp[i] = 1;
+                }
+                max = Math.max(max, dp[i]);
+            }
+            return max;
+
+        }
+    }
+}
