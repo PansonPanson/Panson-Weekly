5fb5f420b45f60a4482072579d8eccc90cb892a4
Panson <PansonPanson@163.com>
2025-04-20 15:27:29 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L309.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L309.java
@@ -16,7 +16,7 @@ public class L309 {
             for(int i = 1; i < n; i++) {
                 dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][2]);
                 dp[i][1] = Math.max(dp[i - 1][1], dp[i - 1][0] - prices[i]);
-                dp[i][2] = Math.max(dp[i - 1][1] + prices[i], dp[i - 1][2]);
+                dp[i][2] = dp[i - 1][1];
             }
             return Math.max(dp[n - 1][0], dp[n - 1][2]);
         }
