b64c1cf46cb455af30812b29b47265a0c2e4cc15
Panson <PansonPanson@163.com>
2025-04-21 17:19:32 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L583.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L583.java
@@ -29,4 +29,25 @@ public class L583 {
             return dp[m][n];
         }
     }
+
+
+    class Solution1{
+        public int minDistance(String word1, String word2) {
+            int m = word1.length(), n = word2.length();
+            int[][] dp = new int[m + 1][n + 1]; // dp[i][j] 表示 word1[0..i-1] 和 word2[0..j-1] 的 LCS 长度
+
+            for (int i = 1; i <= m; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
+                        dp[i][j] = dp[i - 1][j - 1] + 1; // 匹配，LCS + 1
+                    } else {
+                        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]); // 不匹配，取左或上较大者
+                    }
+                }
+            }
+
+            int lcs = dp[m][n];
+            return m + n - 2 * lcs;
+        }
+    }
 }
