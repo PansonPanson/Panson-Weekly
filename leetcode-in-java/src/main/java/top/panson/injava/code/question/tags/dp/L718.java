0d6796a7c8424570fd366048704e51e8ecbd3b69
Panson <PansonPanson@163.com>
2025-04-21 11:29:12 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L718.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/dp/L718.java
@@ -27,4 +27,27 @@ public class L718 {
 
         }
     }
+
+    class Solution2 {
+        public int findLength(int[] nums1, int[] nums2) {
+            // dp[i][j] 以nums1[i], nums2[j] 结尾的最长公共子序列
+            int m = nums1.length;
+            int n = nums2.length;
+            int max = 0;
+            int[] dp = new int[n + 1];
+
+            for(int i = 1; i <= m; i++) {
+                for(int j = n; j > 0; j--) {
+                    if(nums1[i - 1] == nums2[j - 1]) {
+                        dp[j] = dp[j - 1] + 1;
+                    } else {
+                        dp[j] = 0;
+                    }
+                    max = Math.max(dp[j], max);
+                }
+            }
+            return max;
+
+        }
+    }
 }
