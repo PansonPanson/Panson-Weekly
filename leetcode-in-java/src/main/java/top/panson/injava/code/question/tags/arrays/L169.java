76156246db6167f74985f813a8e83d7138cfd866
Panson <PansonPanson@163.com>
2025-04-28 10:23:46 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/arrays/L169.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/arrays/L169.java
@@ -1,5 +1,8 @@
 package top.panson.injava.code.question.tags.arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author Panson
  * @create 2025-03-15
@@ -17,6 +20,34 @@ public class L169 {
             }
             return res;
 
+        }
+    }
+
+    class Solution1 {
+        public int majorityElement(int[] nums) {
+            int res = nums[0];
+            int votes = 0;
+            for(int num : nums) {
+                if(votes == 0) {
+                    res = num;
+                }
+                votes += (num == res ? 1 : -1);
+            }
+            return res;
+        }
+    }
+
+    class Solution2 {
+        public int majorityElement(int[] nums) {
+            int n = nums.length;
+            Map<Integer, Integer> map = new HashMap<>();
+            for(int num : nums) {
+                map.put(num, map.getOrDefault(num, 0) + 1);
+                if(map.get(num) > n/2) {
+                    return num;
+                }
+            }
+            return -1;
         }
     }
 }
