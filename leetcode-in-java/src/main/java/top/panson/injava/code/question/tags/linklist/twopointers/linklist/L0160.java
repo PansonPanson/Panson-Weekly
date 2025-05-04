8a099595916c4c635b771b3bf45cc9c1614b8b99
Panson <PansonPanson@163.com>
2025-05-04 22:05:49 +0800

    update weekly

--- leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/linklist/twopointers/linklist/L0160.java
+++ leetcode-in-java/src/main/java/top/panson/injava/code/question/tags/linklist/twopointers/linklist/L0160.java
@@ -44,4 +44,38 @@ public class L0160 {
 
         }
     }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) {
+     *         val = x;
+     *         next = null;
+     *     }
+     * }
+     */
+    public class Solution1 {
+        public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+            ListNode p1 = headA;
+            ListNode p2 = headB;
+
+            while(p1 != p2) {
+                if(p1 == null) {
+                    p1 = headB;
+                } else {
+                    p1 = p1.next;
+                }
+                if(p2 == null){
+                    p2 = headA;
+                } else {
+                    p2 = p2.next;
+                }
+
+            }
+            return p1;
+
+        }
+    }
 }
